6b558d2212ba0c057bdee1f975849e68112b882e
ngcobosophy <93673208+ngcobosophy@users.noreply.github.com>
2021-11-14 22:22:25 -1000

    Add files via upload

--- Update.php
+++ Update.php
@@ -1,5 +1,10 @@
+<?php
+    session_start();
+    $id = $_SESSION['id'];  
+  
+?>
 <!DOCTYPE html>
-<<html>
+<html>
     <head>
         <title>Update</title>
         <style>
@@ -21,12 +26,15 @@
     <center>
         <h1> Update User Profile </h1>
         <form action="" method="POST">
-            <input type="text" name="txtid" placeholder="Enter customer ID"/><br/>
             <input type="text" name="txtName" placeholder="Enter first name"/><br/>
             <input type="text" name="txtSurname" placeholder="Enter last surname"/><br/>
             <input type="text" name="txtContact" placeholder="Enter phone number"/><br/>
             <input type="text" name="txtEmail" placeholder="Enter email"/><br/>
-            <input type="text" name="txtpasswrd" placeholder="Enter password"/><br/>
+             <input type="text" name="txtpasswrd" placeholder="Enter password"/><br/>
+            <input type="text" name="txtStreetName" placeholder="Enter street name"/><br/>
+            <input type="text" name="txtCity" placeholder="Enter city name"/><br/>
+            <input type="text" name="txtPostalCode" placeholder="Enter postal Code"/><br/>
+            
             <input type="submit" name="update" value="UPDATE DATA"/>
             
         </form>
@@ -37,21 +45,36 @@
 <?php
     include 'connection.php';
     
+    
     if(isset($_POST['update'])){
-          
+
+        $name =$_POST['txtName'];
+        $surname = $_POST['txtSurname'];
+        $cell = $_POST['txtContact'];
+        $mail = $_POST['txtEmail'];
+        $pass = $_POST['txtpasswrd'];
+        $strName=$_POST['txtStreetName'];
+        $ADcity=$_POST['txtCity'];
+        $PlCode=$_POST['txtPostalCode'];
+            
    
-              //$id= $_POST['id'];
-              $update = "UPDATE 'customer' SET customer_f_name = '$_POST[txtName]',customer_l_name= '$$_POST[txtSurname]',customer_phone = '$_POST[txtContact]',customer_password = '$_POST[txtpasswrd]',customer_email = '$_POST[txtEmail]' WHERE customer_id = '$_POST[txtid]' ";
-              $sql_run = mysqli_query($conn,$update);
-              
-              if($sql_run)
-              {
+
+        $update = "UPDATE customer SET customer_f_name='$name',customer_l_name='$surname',customer_phone='$cell', 
+            customer_email='$mail',customer_password='$pass' WHERE customer_id = '$id'";
+        $sql_run = mysqli_query($conn,$update);
+        
+        $add ="UPDATE customer_address SET cust_add_postalcode ='$PlCode',cust_add_city='$ADcity',cust_street='$strName' WHERE customer_id= '$id'";
+        $add_run = mysqli_query($conn,$add); 
+        
+        if($sql_run && $add_run)
+        {
                   
-                echo '<script type="text/javascript">alert("Data updated")</script>';
+            echo '<script type="text/javascript">alert("Data updated")</script>';
                   
-              } else
-              {
-                  echo '<script type="text/javascript">alert("Data not updated")</script>';
-              }
+        } else
+            {
+                echo '<script type="text/javascript">alert("Data not updated")</script>';
+            }
     }
+    
 ?>
